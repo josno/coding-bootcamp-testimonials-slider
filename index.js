ee07f25f7ea11d02d67fe30d205a1d4a362f3161
josno <jnsenoren@gmail.com>
2020-09-16 04:23:37 +0400

    complete javascript

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-const list = document.getElementsByTagName("section");
+const btn = document.querySelectorAll("svg");
+
+var toggle = (currentElement, nextElement) => {
+	currentElement.classList.toggle("do-not-display");
+	nextElement.classList.toggle("do-not-display");
+	nextElement.classList.add("testimonial-1");
+};
+
+var goNext = (l, c, n) => {
+	if (c.id === (l.length - 1).toString()) {
+		c = l[0];
+		n = l[1];
+	}
+
+	toggle(c, n);
+};
+
+var goBack = (l, c, n) => {
+	const beginningOfList = 0;
+	if (c.id === beginningOfList.toString()) {
+		c = l[0];
+		n = l[1];
+	}
+
+	toggle(c, n);
+};
+
+btn.forEach((i) => {
+	i.addEventListener("click", (event) => {
+		var list = document.querySelectorAll("section");
+		var current = document.querySelectorAll("section")[0];
+		var next = document.querySelectorAll("section")[1];
+
+		if (i.classList.contains("next")) {
+			goNext(list, current, next);
+		} else {
+			goBack(list, current, next);
+		}
+	});
+});
